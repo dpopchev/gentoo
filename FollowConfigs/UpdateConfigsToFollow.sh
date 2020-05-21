ebd5209d98a22b1a41a2d26ba545aeb4cb318f55
dimitar <dpopchev@gmail.com>
2020-05-21 18:39:01 +0300

    commit before push for first time for compatibility reasons

--- FollowConfigs/UpdateConfigsToFollow.sh
+++ FollowConfigs/UpdateConfigsToFollow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # place to store files to follow absolute paths
-LIST_CONFIGS=/home/dimitar/projects/Gentoo/FollowFiles/ConfigsToFollow
+LIST_CONFIGS=/home/dimitar/projects/Gentoo/FollowConfigs/ConfigsToFollow
 
 # current working directory
 CWD=$(pwd)
@@ -9,15 +9,15 @@ CWD=$(pwd)
 # program description
 DESCRIPTION="\
 $(basename $0) is a script to help maintain list of files/directories. \
-Each of those are to be backup on regular intervals, but this is \
-not to be covered by the present script. \
+Each of those are to be backup on regular intervals, action not to be covered \
+by the present script. \
 \n\n Flag usage: "
 
 # list of valid flags and their description (long version as a key)
 declare -A FLAGS
 FLAGS+=(["--help"]="show this message")
 FLAGS+=(["--add"]="add file/directory to follow list")
-FLAGS+=(["--remove"]="remove file/directory to follow list")
+FLAGS+=(["--delete"]="delete file/directory to follow list")
 FLAGS+=(["--update"]="update current record in follow list with new one")
 FLAGS+=(["--get"]="get file/directory if present")
 FLAGS+=(["--list"]="list all currently followed files/configs")
@@ -27,19 +27,21 @@ FLAGS+=(["--edit"]="edit $LIST_CONFIGS in place with your preferred editor: $EDI
 declare -A FLAGS_PAIRS
 FLAGS_PAIRS+=(["--help"]="-h")
 FLAGS_PAIRS+=(["--add"]="-a")
-FLAGS_PAIRS+=(["--remove"]="-r")
+FLAGS_PAIRS+=(["--delete"]="-d")
 FLAGS_PAIRS+=(["--update"]="-u")
 
 usage(){
+    # function to generate usage of the script
 
     # setting max line length
     local -i maxLineLen=81
 
-    # hard code local delimiter to some amount of spaces
-    local delimiter="  "
+    # hard code indentation
+    local indent="  "
 
     # generate the flag usage
     _createFlagsDescription(){
+        # function to generate flag description
 
         # go over all keys to check which is the longest
         local -i maxFlagLen=0
@@ -59,12 +61,12 @@ usage(){
         done
 
         # the rest of the space will be used for the description
-        local DescriptionSpace=$(expr $maxLineLen - $maxFlagLen - ${#delimiter})
+        local DescriptionSpace=$(expr $maxLineLen - $maxFlagLen - ${#indent})
 
-        # create line formatter, which is indented with delimiter amount of
+        # create line formatter, which is indented with indent amount of
         # space, reserves the longest possible amount of flag space and uses the
         # rest for description
-        local line_formater="${delimiter} %-${maxFlagLen}s${delimiter}%-${DescriptionSpace}s\n"
+        local line_formater="${indent} %-${maxFlagLen}s${indent}%-${DescriptionSpace}s\n"
 
         # iterate over all long version key flags
         for flag in "${!FLAGS[@]}"; do
@@ -90,14 +92,13 @@ usage(){
             local _is_any_description_pending=false
 
             # iterate over the flag description word by word
-            # if we exceed the space left for the description, we print and null
-            # the description local variable
+            # if we exceed the space left for the description, we print it
+            # and null the description local variable
             for word in ${FLAGS[$flag]}; do
                 _description+=" $word"
                 if [[ ${#_description} -lt ${DescriptionSpace} ]]; then
                     # rise mark for pending description
                     _is_any_description_pending=true
-                    :
                 else
                     # check that we have printed inside the loop
                     _is_firstline_printed=true
@@ -173,184 +174,97 @@ $(_createFlagsDescription)
 HELP_USAGE
 }
 
-editFile(){
-    $EDITOR $LIST_FILES
-}
-
 cleanEmptyLines(){
-# remove empty lines from the storage file
-    sed -i '/^$/d' $LIST_FILES
+    # clean empty lines from the storage file, if any present
+    sed -i '/^$/d' $LIST_CONFIGS
 }
 
-normalizePath(){
-# all paths in LIST_FILES are absolute, thus here we normalize the input path
-#   input -- $1, string, file path to normalize
-#   output -- string, normalize path with exit code 0 if successful
-#               normalized path and exit code 2 if file does not exists
+addConfigs(){
+    # check if item is valid file/path and if it is add it to the follow file
 
-    # if file path is local dir, expand it to current working directory
-    [[ $(dirname "$1") == "." ]] && NORM_FILEPATH="${PWD}/$(basename $1)" \
-        || NORM_FILEPATH=$1
+    local -r item=$1
 
-    # check if file exists
-    [[ -f $NORM_FILEPATH ]] && echo $NORM_FILEPATH && return 0 \
-        || echo "$NORM_FILEPATH does not exist" && return 2
+    grep -q $item $LIST_CONFIGS
+    local -r item_present=$?
+
+    # if the config item is already present, inform and skip adding
+    if [[ $item_present -eq 0 ]]; then
+        echo "$item... already present, skipping"
+    else
+        echo $item >> $LIST_CONFIGS
+    fi
+
+    return 0
 }
 
-addConfig(){
+checkIsFlag(){
+    # check if present argument is a valid flag
+    # return 0 argument is either a long or short flag
+    # return 1 if it is neither
 
-    NEW_FILE=$(normalize_FilePath $NEW_FILE)
+    grep -wq -- "${1}" <(echo "${!FLAGS[*]}")
+    local check_short=$?
 
-    # file does not exist, exit the script
-    [[ $? -eq 2 ]] && echo "$NEW_FILE" && exit 0
+    grep -wq -- "${1}" <(echo "${FLAGS_PAIRS[*]}")
+    local check_long=$?
 
-    # check how many occurrences there are in LIST_FILES
-    GREP_R="$(grep -cs $NEW_FILE $LIST_FILES)"
+    # result variable, presume argument is not a valid flag
+    local r=1
 
-    # if grep exits with unsuccessful exit code infrom
-    [[ $? -eq 2 ]] && echo "Unexpected grep result: $GREP_R" && exit 2
-    case $GREP_R in
-        1)      echo "File already followed"
-                ;;
-        0)      echo $NEW_FILE >> $LIST_FILES
-                ;;
-        *)      echo "Unexpected case, $GREP_R"
-                ;;
-    esac
+    if [ $cehck_short -eq 0 ] || [ $check_long -eq 0 ]; then
+        r=0
+    else
+        r=1
+    fi
+
+    return r
 }
 
-del_file(){
-# after normalizing the DEL_FILE check if already present
-# if not -- complain, if yes -- remove
-
-    DEL_FILE=$(normalize_FilePath $DEL_FILE)
-
-    # file does not exist, exit the script
-    [[ $? -eq 2 ]] && echo "$DEL_FILE" && exit 0
-
-    # check how many occurrences there are in LIST_FILES
-    GREP_R="$(grep -cs $DEL_FILE $LIST_FILES)"
-
-    # if grep exits with unsuccessful exit code infrom
-    [[ $? -eq 2 ]] && echo "Unexpected grep result: $GREP_R" && exit 2
-    case $GREP_R in
-        1)      sed -i "\|$DEL_FILE|d" $LIST_FILES
-                ;;
-        0)      echo "File not followed yet"
-                ;;
-        *)      echo "Unexpected case, $GREP_R"
-                ;;
-    esac
-}
-
-Delete_file(){
-# delete file without taking into account path
-
-    DEL_FILE=$(basename $DEL_FILE)
-
-    # check how many occurrences there are in LIST_FILES
-    GREP_R="$(grep -cs $DEL_FILE $LIST_FILES)"
-
-    # if grep exits with unsuccessful exit code infrom
-    [[ $? -eq 2 ]] && echo "Unexpected grep result: $GREP_R" && exit 2
-    case $GREP_R in
-        1)      sed -i "\|$DEL_FILE|d" $LIST_FILES
-                ;;
-        0)      echo "File not followed yet"
-                ;;
-        *)      echo "Unexpected case, $GREP_R"
-                ;;
-    esac
-}
-
-#updt_file(){
-## if FILE present, replace it with REPLACEMENT
-## if not present or no update provided -- add it
-## if any ambiguity detected -- inform
-#    FILE=`basename $FILE`
-#    REPLACEMENT=`basename $REPLACEMENT`
-#    # if no replacement given, add_file
-#    [[ ! -z $REPLACEMENT ]]
-#    case `grep -c "${PWD}/${FILE}$" $LIST_FILES` in
-#        0)      # File to update missing, will add it"
-#                echo "File to update missing, adding it"
-#                FILE=$_FILE
-#                add_file
-#                ;;
-#        1)      # One occurrence of file, replace
-#                sed -i "|${_FILE}|${PWD}/${REPLACEMENT}|g" $LIST_FILES
-#
-#    if grep -Fxq $FILE $LIST_FILES; then
-#        sed -i "s|${FILE}|${FILE1}|g" $LIST_FILES
-#    else
-#        echo "File to update missing, adding it"
-#        add_file
-#    fi
-#}
-
-get_file(){
-# check if file is present, and if so return the path
-    echo `grep $FILE $LIST_FILES`
-}
-
-list_files(){
-# clean list of files file from empty lines and print
-    cat $LIST_FILES
-}
-
-# print help if no arguments
+# print usage if no arguments
 if [[ $# -eq 0 ]]; then
     usage
+
 # go through all flags
 else
     # make sure we do not have empty lines in the file
-    clean_EmptyLines
-    while [[ $1 != "" ]]; do
-        case $1 in
-            -a | --add)     # check if there is file to add
-                            [[ ! -z $2 ]] && NEW_FILE=$2 && add_file \
-                                || echo "No file to add" && exit $RES
-                            ;;
-            -d | --delete)  # check if there is file to delete
-                            [[ ! -z $2 ]] && DEL_FILE=$2 && del_file \
-                                || echo "No file to delete" && exit $RES
-                            ;;
-            -u | --update)  # check if there is file to update with
-                            [[ ! -z $3 ]] && REPLACEMENT=$3 || echo "No file to update with"
 
-                            # check if there is file to update
-                            [[ ! -z $2 ]] && FILE=$2 && updt_file \
-                                || echo "No file to update"
-                            exit $RES
-                            ;;
-            -g | --get)     # if file is followed get its full record
-                            [[ ! -z $2 ]] && FILE=$2 && get_file \
-                                || echo "No file to get"
-                            exit $RES
-                            ;;
-            -l | --list)    list_files
-                            exit $RES
-                            ;;
-            -e | --edit)    edit_file
-                            exit $RES
-                            ;;
-            -h | --help)    usage
-                            exit $RES
-                            ;;
-            -D | \
-            --Delete_file)  # check if there is file to delete
-                            [[ ! -z $2 ]] && DEL_FILE=$2 && Delete_file \
-                                || echo "No file to delete" && exit $RES
-                            ;;
-            *)              echo "$1 not recognized"
-                            echo "see help for available options"
-                            usage
-                            exit $RES
-                            ;;
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+
+            # print usage infomration
+            --help|${FLAGS_PAIRS["--help"]})
+                usage
+                exit 0
+                ;;
+
+            # add file/directory to follow list
+            --add|${FLAGS_PAIRS["--add"]})
+                shift 1
+
+                # iterate until reaching a valid flag or arguments end
+                local isNextArgFlag=1
+                while [ "$#" -gt 0 ] && [
+                    && ! grep -wq -- "${1}" <(echo "${!FLAGS[*]}") \
+                    && ! grep -wq -- "${1}" <(echo "${FLAGS_PAIRS[*]}"); do
+
+                    item=$(readlink -f $1)
+
+                    echo $item
+
+                    # check if item exists, if so added it
+                    #if [[ -e $item ]]; then
+                    #    addConfigs $item
+                    #else
+                    #    echo "$item does not exists, skipping..."
+                    #fi
+
+                    shift 1
+                done
+                ;;
+            *)
+                echo "$1 unknown argument, please consult with help..."
+                exit 1
+                ;;
         esac
-        # should not reach this point
-        exit 2
     done
 fi
-
-exit $RES
--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # Introduction
 
-I have long dreamed to systematize all information I gather when
-solving day-to-day issues with my Gentoo system. Despite the increased
-documentation quality of the Gentoo wiki, not to forget the Arch wiki, plus the
-improved system tools, it is still a lot of effort to track down the source of
-particular option or hack I have used and thus the reason behind creating this
-repository.
+Since my favourite Linux distribution, thanks to my father, is Gentoo
+for long I was relaying on
